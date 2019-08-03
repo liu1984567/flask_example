b4eb161474d371b5b889a15b9300954dd745a70f
liudh <liudh@inspur.com>
2019-08-03 23:09:11 +0800

    Add requirement.txt, test_basics

--- manager2.py
+++ manager2.py
@@ -19,5 +19,12 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
+@manager.command
+def test():
+    """run the unit test"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == '__main__': 
     manager.run()
