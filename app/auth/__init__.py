f562b7ac0506c7d6c0d28e1f345f963664b91fb8
liudh <liudh@inspur.com>
2019-08-05 23:20:38 +0800

    Modify:1.base html and log status        2.add user login(need test)

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -7,5 +7,5 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-#from . import views
+from . import views
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -3,12 +3,25 @@
 
     Implements all views in the app.
 """
-from flask import render_template
+from flask import render_template,redirect,url_for,flash
+from flask_login import login_required
 from . import auth
 from .. import db
 from ..models import User
+from .forms import LoginForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('/auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            return redirect(url_for('main.index'))
+        flash('Invalidate email or password')
+    return render_template('auth/login.html', form=form)
+
+@auth.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
