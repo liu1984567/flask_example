eb9cc1a51ced5bd5892081fd61ffc68ad94f65bb
liudonghao <liudonghao@gtlybj.com>
2019-08-08 17:31:12 +0800

    daily commit

--- notify_stockinfo.py
+++ notify_stockinfo.py
@@ -6,10 +6,13 @@
 import os
 import time
 from urllib.request import urlopen
+import smtplib
+from email.mime.text import MIMEText
+
 
 HTML_STR = 'http://hq.sinajs.cn/?func=getData._hq_cron();&list='
 SEL_LIST    = ['sh000300', 'sh000016', 'sh000905', 'sh000947', 'sh000991', 'sh000912']  
-DATA_FILE = 'sina_finance_data_0.csv'
+RCV_EMAIL = 'liu198456@126.com'
 
 def onlyNum(s,oth=','):
     s2 = s.lower();
@@ -19,11 +22,28 @@ def onlyNum(s,oth=','):
             s = s.replace(c, '')
     return s;
 
+def send_mail(to, subject, str_content):
+    from_email = os.environ.get('MAIL_USERNAME')
+    mail_password = os.environ.get('MAIL_PASSWORD')
+    mail_server = os.environ.get('MAIL_SERVER')
+    mail_srvport = os.environ.get('MAIL_SERVER_PORT')
+    msg = MIMEText(str_content)
+    msg['From'] = from_email
+    msg['To'] = to 
+    msg['Subject'] = subject
+    email_server = smtplib.SMTP(mail_server, 25)
+    email_server.set_debuglevel(1)
+    email_server.login(from_email, mail_password)
+    email_server.sendmail(from_email, [to], msg.as_string())
+    email_server.quit()
+
 def main():
     global SEL_LIST;
     global HTML_STR;
     count = len(SEL_LIST);
     strtmp = '';
+    str_message = '';
+    str_content = '';
     for i in range(0,count):
         strtmp = strtmp + SEL_LIST[i];
         if i < count - 1:
@@ -31,34 +51,28 @@ def main():
     HTML_STR = HTML_STR + strtmp;
     print(HTML_STR);
     
-    global DATA_FILE
-    if not os.path.exists(DATA_FILE):
-        fo = open(DATA_FILE, "w+");
-        fo.write("date, " + strtmp + "\n");
-        fo.close();
 
-    fo = open(DATA_FILE, "a+");
     strdate = time.strftime("%Y.%m.%d", time.localtime())
-    fo.write(strdate+", ");
+    str_message = strdate + ':\n';
 
     response = urlopen(HTML_STR)
+    response_encoding = response.headers.get_content_charset()
+    print(response_encoding)
     content = response.read()
-    str_content = content.decode()# error decode
-    print(str_ontent)
+    #print(content)
+    str_content = content.decode(response_encoding)
+    print(str_content)
     lines = str_content.split('\n')
     for line in lines:
         print(line)
-    count = content.count('\n');
-    for strtmp in content.splitlines(count):
-        #print strtmp
-        s1 = onlyNum(strtmp)
+        s1 = onlyNum(line)
         print(s1)
         if s1.count(',') > 3:
-            print (s1.split(',')[3] )
-            fo.write(s1.split(',')[3] + ", ");
-    
-    fo.write('\n');
-    fo.close()
+            str_val = s1.split(',')[3];
+            print (str_val)
+            str_message = str_message + str_val + ', ';
+    print (str_message)         
+    send_mail(RCV_EMAIL,  'stock chance', str_message) 
 
 
 if __name__ == '__main__':
